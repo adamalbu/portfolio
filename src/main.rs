50f6cca262d5de7a5761485258aad2122ce1d865
adamalbu <45970222+adamalbu@users.noreply.github.com>
2025-07-18 16:05:00 +0100

    added project cards

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,33 @@
 use yew::prelude::*;
 
+#[derive(Properties, PartialEq)]
+struct ProjectCardProps {
+    name: String,
+    description: String,
+}
+
+#[function_component(ProjectCard)]
+fn project_card(props: &ProjectCardProps) -> Html {
+    html! {
+        <div class="rounded-md w-55 p-4 bg-white hover:z-10 transition filter drop-shadow-lg hover:drop-shadow-2xl hover:scale-105">
+            <h1 class="text-center font-medium text-lg">{ &props.name }</h1>
+            <hr />
+            <p class="text-left">{ &props.description }</p>
+        </div>
+    }
+}
+
 #[function_component(App)]
 fn app() -> Html {
     html!{
-        <h1 class="text-3xl font-medium">{ "Hello from Rust!" }</h1>
+        <>
+        <h1 class="text-3xl text-center font-medium font-mono">{ "adamprojects.net" }</h1>
+        <hr class="larger"/>
+        <div class="flex space-x-4">
+            <ProjectCard name="Project" description="Lorem ipsum" />
+            <ProjectCard name="Longer Project" description="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua." />
+        </div>
+        </>
     }
 }
 
